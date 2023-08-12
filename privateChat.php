a5b8e585191866b67f451a2a512b507696654d38
Saeed Sarailoo <ssarailoo@yahoo.com>
2023-08-12 20:15:08 +0330

    refactor: private chats connet to the database

--- privateChat.php
+++ privateChat.php
@@ -1,57 +1,27 @@
 <?php
+require_once "DatabaseConnection.php";
 session_start();
 $userLogged = $_SESSION['user'];
 date_default_timezone_set("Asia/Tehran");
+$pdo = DatabaseConnection::getInstance()->getConnection();
 $to = $_GET['to'];
-$users = json_decode(file_get_contents('./storage/users.json'), true);
-foreach ($users
-         as $i => $user) {
-    if ($user['hashedUsername'] == $to) {
-        $to = ["username" => $user['username'],
-            "name" => $user['name'],
-            "email" => $user['email'],
-            "password" => $user['password'],
-            "profilePic" => $user['profilePic'],
-            "bio" => $user['bio'],
-            "isAdmin" => $user['isAdmin'],
-            "isBlocked" => $user['isBlocked'],
-            "friends" => $user['friends'],
-            "hashedUsername" => $user['hashedUsername']
-        ];
-
-    }
-}
-
-$number=1;
-if (is_file('./storage/privateChat.json')) {
-    $jsonChatArray = json_decode(file_get_contents('./storage/privateChat.json'), true);
-    $number=count($jsonChatArray)+1 ;
-
-}
+$stmt=$pdo->prepare('SELECT id from users where hashed_username=:to');
+$stmt->execute(['to'=>$to]);
+$to_id=$stmt->fetch()['id'];
 $user = $userLogged['username'];
-$user .= $userLogged['isAdmin'] ? " (admin):" : " :";
+$user .= $userLogged['is_admin'] ? " (admin):" : " :";
 if (isset($_POST['send'])) {
     $privateChatErrors = array();
-    if ($userLogged['isBlocked']) {
+    if ($userLogged['is_blocked']) {
         $privateChatErrors['blocked'] = "You are blocked from chat!";
     } else {
         if (isLong($_POST['privateMessage'])) {
             $privateChatErrors['messageLength'] = "You must add 1 to 100 characters as message";
         } else if (!empty($_POST['privateMessage'])) {
             $message = $_POST['privateMessage'];
-            $message =  stripslashes(htmlspecialchars($message));
-
-            $chatInfo = [
-                'from' => $userLogged['username'],
-                'fromProfile'=>$userLogged['profilePic'],
-                'number' => $number,
-                'time' => date("y/m/d h:i:s"),
-                'to' => $to['username'],
-                'toProfile'=>$to['profilePic'],
-                'message' => $message
-            ];
-            $jsonChatArray[] = $chatInfo;
-            file_put_contents('./storage/privateChat.json', json_encode($jsonChatArray, JSON_PRETTY_PRINT));
+            $message = stripslashes(htmlspecialchars($message));
+            $stmt = $pdo->prepare('INSERT INTO private_chats(message,send_at,from_id,to_id )VALUES (:message,:send_at,:from_id,:to_id)');
+            $stmt->execute(['message' => $message, 'send_at' => date("y/m/d h:i:s"), 'from_id' => $userLogged['id'], 'to_id' => $to_id]);
 
 
         }
@@ -63,41 +33,21 @@ if (isset($_POST['send'])) {
         } else if ($size > 0) {
             move_uploaded_file($image['tmp_name'], "./storage/pictures/chatPics/private/$name");
             $imageDir = "./storage/pictures/chatPics/private/$name";
-            $data =  "<img style='width: 150px;height: 150px' src='$imageDir' >";
-            $chatInfo = [
-                'from' => $userLogged['username'],
-                'fromProfile'=>$userLogged['profilePic'],
-                'number' => $number,
-                'time' => date("y/m/d h:i:s"),
-                'to' => $to['username'],
-                'toProfile'=>$to['profilePic'],
-                'message' => $data
-            ];
-            $jsonChatArray[] = $chatInfo;
-            file_put_contents('./storage/privateChat.json', json_encode($jsonChatArray, JSON_PRETTY_PRINT));
-
-
+            $data = "<img style='width: 150px;height: 150px' src='$imageDir' >";
+            $stmt = $pdo->prepare('INSERT INTO private_chats( message,send_at,from_id,to_id )VALUES (:message,:send_at,:from_id,:to_id)');
+            $stmt->execute(['message' => $data, 'send_at' => date("y/m/d h:i:s"), 'from_id' => $userLogged['id'], 'to_id' => $to_id]);
         }
-
     }
 
 
 }
 if (isset($_POST['deletePrivate'])) {
-    $number = $_POST['deletePrivate'];
-    $chats = json_decode(file_get_contents('./storage/privateChat.json'), true);
-    foreach ($chats as $index => $chat) {
-        if ($index + 1 == $number) {
-            $chat['message'] = '';
-            $chat['deletedTime'] = date("y/m/d h:i:s");
-            $chats[$index] = $chat;
-            break;
-        }
-    }
-    file_put_contents('./storage/privateChat.json', json_encode($chats, JSON_PRETTY_PRINT));
+    $id = $_POST['deletePrivate'];
+    $stmt = $pdo->prepare('DELETE FROM private_chats WHERE id=:id ');
+    $stmt->execute(['id'=>$id]);
 
 }
-if (isset($_POST['back'])){
+if (isset($_POST['back'])) {
     header('location:mainPage.php');
 }
 /**
@@ -179,23 +129,23 @@ function isLargImage(int $size): bool
             <div class="col-md-6 col-lg-7 col-xl-7 second">
 
                 <?php
-                if (file_exists("./storage/privateChat.json") && filesize("./storage/privateChat.json") > 0) {
-                    $chatsInfos = json_decode(file_get_contents("./storage/privateChat.json"), true);
-                    foreach ($chatsInfos as $index => $chat) {
-                        $number = $chat['number'];
-                if (($chat['from'] == $userLogged['username'] && $chat['to'] == $to['username']) || ($chat['from'] == $to['username'] && $chat['to'] == $userLogged['username'])){
-                if ($chat['from'] == $userLogged['username']){
-                if (empty($chat['message']))
-                    continue;
-                    $profile = $chat['fromProfile'];
-                    $username = $chat['from'];
-                    $time = $chat['time'];
-                            if ($userLogged['username'] == $chat['from'])
-                                $chat['message'] =  $chat['message']. " <form action='' method='post'><button type='submit'  name='deletePrivate' value='$number'>Delete</button></form>";
+              $stmt= $pdo->prepare('SELECT * FROM private_chats WHERE (from_id=:from_id AND to_id=:to_id)OR (from_id=:to_id AND to_id=:from_id) ');
+               $stmt->execute(['from_id'=>$userLogged['id'],'to_id'=>$to_id]);
+              $chatsInfos= $stmt->fetchAll();
+                foreach ($chatsInfos
 
+                as $index => $chat) {
+                $id = $chat['id'];
+               {
+                if ($chat['from_id'] == $userLogged['id']){
 
-
-
+               $stmt= $pdo->prepare("SELECT profile_pic,username from users WHERE id=:id ");
+               $stmt->execute(['id'=>$userLogged['id']]);
+               $userData=$stmt->fetch();
+                $profile = $userData['profile_pic'];
+                $username = $userData['username'];
+                $time = $chat['send_at'];
+                    $chat['message'] = $chat['message'] . " <form action='' method='post'><button type='submit'  name='deletePrivate' value='$id'>Delete</button></form>";
                 ?>
                 <ul class="list-unstyled text-white">
                     <li class="d-flex justify-content-between mb-4 notLogged">
@@ -215,37 +165,39 @@ function isLargImage(int $size): bool
                         <img src="<?= $profile ?>" alt="avatar"
                              class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
                     </li>
-                    <?php }   else {
-                        if (empty($chat['message']))
-                            continue;
-                        $profile = $chat['fromProfile'];
-                        $username = $chat['from'];
-                        $time = $chat['time'];
-                        if ($userLogged['username'] == $chat['from'])
-                            $chat['message'] =  $chat['message']. " <form action='' method='post'><button type='submit'  name='deletePrivate' value='$number'>Delete</button></form>";
+                    <?php } else {
+                        $stmt= $pdo->prepare("SELECT profile_pic,username from users WHERE id=:to_id ");
+                        $stmt->execute(['to_id'=>$to_id]);
+                        $userData=$stmt->fetch();
+                        $profile = $userData['profile_pic'];
+                        $username = $userData['username'];
+                        $time = $chat['send_at'];
+                        if ($userLogged['username'] == $chat['from_id'])
+                            $chat['message'] = $chat['message'] . " <form action='' method='post'><button type='submit'  name='deletePrivate' value='$id'>Delete</button></form>";
 
                         ?>
-                    <li class="d-flex justify-content-between mb-4 logged">
-                        <img src="<?= $profile ?>" alt="avatar"
-                             class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
-                        <div class="card mask-custom kadr">
-                            <div class="card-header d-flex justify-content-between p-3"
-                                 style="border-bottom: 1px solid rgba(255,255,255,.3);">
-                                <p class="fw-bold mb-0"><?= $username ?></p>
-                                <p class="text-light small mb-0"><i class="far fa-clock"></i> <?= $time ?> </p>
+                        <li class="d-flex justify-content-between mb-4 logged">
+                            <img src="<?= $profile ?>" alt="avatar"
+                                 class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
+                            <div class="card mask-custom kadr">
+                                <div class="card-header d-flex justify-content-between p-3"
+                                     style="border-bottom: 1px solid rgba(255,255,255,.3);">
+                                    <p class="fw-bold mb-0"><?= $username ?></p>
+                                    <p class="text-light small mb-0"><i class="far fa-clock"></i> <?= $time ?> </p>
+                                </div>
+                                <div class="card-body">
+                                    <p class="mb-0">
+                                        <?= $chat['message'] ?>
+                                    </p>
+                                </div>
                             </div>
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    <?= $chat['message'] ?>
-                                </p>
-                            </div>
-                        </div>
-                    </li>
+                        </li>
 
 
                     <?php }
                     }
-                    }} ?>
+
+                    } ?>
                     <li class="mb-3">
                         <div class="form-outline form-white">
                             <form action="" method="post" enctype="multipart/form-data">
@@ -259,11 +211,12 @@ function isLargImage(int $size): bool
                                 <button type="submit" name="send" class="btn btn-light btn-lg btn-rounded float-end">
                                     Send
                                 </button>
-                                <h3 class="text-danger"><?php if (!empty($privateChatErrors)){
-                                    foreach ($privateChatErrors as $error)
-                                    echo $error."<br>";
+                                <h3 class="text-danger"><?php if (!empty($privateChatErrors)) {
+                                        foreach ($privateChatErrors as $error)
+                                            echo $error . "<br>";
                                     } ?></h3>
-                                <button class="btn btn-lg btn-secondary text-white" type="submit" name="back" >back</button>
+                                <button class="btn btn-lg btn-secondary text-white" type="submit" name="back">back
+                                </button>
                             </form>
 
                         </div>
